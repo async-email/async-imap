c43612ae86ddd4911ba15cfe647346c3dd2f6b2c
holger krekel <holger@merlinux.eu>
2019-11-10 16:29:37 +0100

    nicify example: streamline printing and match on IdleResponse results

--- examples/idle.rs
+++ examples/idle.rs
@@ -1,5 +1,7 @@
 use async_imap::error::{Error, Result};
 use async_std::prelude::*;
+// use async_std::io;
+use async_imap::extensions::idle::IdleResponse::*;
 use async_std::task;
 use std::env;
 use std::time::Duration;
@@ -23,46 +25,64 @@ async fn fetch_and_idle(imap_server: &str, login: &str, password: &str) -> Resul
     // we pass in the imap_server twice to check that the server's TLS
     // certificate is valid for the imap_server we're connecting to.
     let client = async_imap::connect((imap_server, 993), imap_server, &tls).await?;
-    println!("** connected to {}:{}", imap_server, 993);
+    println!("-- connected to {}:{}", imap_server, 993);
 
     // the client we have here is unauthenticated.
     // to do anything useful with the e-mails, we need to log in
     let mut session = client.login(login, password).await.map_err(|e| e.0)?;
-    println!("** logged in a {}", login);
+    println!("-- logged in a {}", login);
 
     // we want to fetch some messages from the INBOX
     session.select("INBOX").await?;
-    println!("** INBOX selected");
+    println!("-- INBOX selected");
 
-    // fetch messages
-    let msg_stream = session.fetch("1:3", "(FLAGS BODY.PEEK[])").await?;
+    // fetch flags from all messages
+    let msg_stream = session.fetch("1:*", "(FLAGS )").await?;
     let msgs = msg_stream.collect::<Vec<_>>().await;
-    println!("** number of fetched msgs: {:?}", msgs.len());
+    println!("-- number of fetched msgs: {:?}", msgs.len());
 
     // init idle session
-    println!("** initting idle");
+    println!("-- initializing idle");
     let mut idle = session.idle();
     idle.init().await?;
 
-    println!("** idle async wait");
+    println!("-- idle async wait");
     let (idle_wait, interrupt) = idle.wait();
 
+    /*
+    let stdin = io::stdin();
+    let mut line = String::new();
+    stdin.read_line(&mut line).await?;
+    println!("-- read line: {}", line);
+    */
+
     task::spawn(async move {
-        println!("** thread: waiting for 30s");
+        println!("-- thread: waiting for 30s");
         task::sleep(Duration::from_secs(30)).await;
-        println!("** thread: waited 30 secs, now interrupting idle");
+        println!("-- thread: waited 30 secs, now interrupting idle");
         drop(interrupt);
     });
 
     let idle_result = idle_wait.await;
-    println!("** idle msg: {:#?}", &idle_result);
+    match idle_result {
+        ManualInterrupt => {
+            println!("-- IDLE manually interrupted");
+        }
+        Timeout => {
+            println!("-- IDLE timed out");
+        }
+        NewData(data) => {
+            let s = String::from_utf8(data.raw.to_vec()).unwrap();
+            println!("-- IDLE data:\n{}", s);
+        }
+    }
 
     // return the session after we are done with it
-    println!("** sending DONE");
+    println!("-- sending DONE");
     let mut session = idle.done().await?;
 
     // be nice to the server and log out
-    println!("** logging out");
+    println!("-- logging out");
     session.logout().await?;
     Ok(())
 }
