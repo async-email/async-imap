0054f904da831a3eefd47b0782626891cacc9a60
link2xt <link2xt@testrun.org>
2023-04-11 18:04:21 +0000

    Fix examples

--- examples/src/bin/gmail_oauth2.rs
+++ examples/src/bin/gmail_oauth2.rs
@@ -1,5 +1,10 @@
-use async_imap::error::Result;
+use anyhow::Result;
 use futures::StreamExt;
+
+#[cfg(feature = "runtime-async-std")]
+use async_std::net::TcpStream;
+#[cfg(feature = "runtime-tokio")]
+use tokio::net::TcpStream;
 
 struct GmailOAuth2 {
     user: String,
@@ -27,14 +32,16 @@ async fn main() -> Result<()> {
     let domain = "imap.gmail.com";
     let port = 993;
     let socket_addr = (domain, port);
+    let tcp_stream = TcpStream::connect(socket_addr).await?;
     let tls = async_native_tls::TlsConnector::new();
-    let client = async_imap::connect(socket_addr, domain, tls).await?;
+    let tls_stream = tls.connect(domain, tcp_stream).await?;
+    let client = async_imap::Client::new(tls_stream);
 
     let mut imap_session = match client.authenticate("XOAUTH2", &gmail_auth).await {
         Ok(c) => c,
         Err((e, _unauth_client)) => {
             println!("error authenticating: {}", e);
-            return Err(e);
+            return Err(e.into());
         }
     };
 
